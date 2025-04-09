b7a69e51431e90318b41543a015a7e2f4e29d7b7
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2025-04-09 09:59:14 +0200

    Update creation_database.sql

--- creation_database.sql
+++ creation_database.sql
@@ -1,47 +1,57 @@
+CREATE TABLE dividende (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    pourcentage FLOAT NOT NULL,
+    date_distribution DATE
+)
+
 CREATE TABLE action (
-    name VARCHAR(255) PRIMARY KEY,
-    price FLOAT,
-    dividende_part FLOAT,
-    dividend_payment_date DATE
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL,
+    symbole VARCHAR(255) NOT NULL UNIQUE,
+    price DECIMAL(10,2) NOT NULL,
+    id_dividende INT,
+    FOREIGN KEY (id_dividende) REFERENCES dividende(id)
  );
  
- CREATE TABLE users (
-     email VARCHAR(255) PRIMARY KEY,
-     pseudo VARCHAR(255),
-     mdp VARCHAR(255),
-     solde FLOAT
+ CREATE TABLE utilisateur (
+     id INT PRIMARY KEY AUTO_INCREMENT,
+     pseudo VARCHAR(255) UNIQUE NOT NULL,
+     email VARCHAR(255) UNIQUE NOT NULL,
+     mdp VARCHAR(255) NOT NULL,
+     solde FLOAT DEFAULT 10000.00,
+     date_inscription TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE global_wallet (
-    email_user VARCHAR(255),
-    action_name VARCHAR(255),
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_user VARCHAR(255),
+    id_action VARCHAR(255),
     quantity INT,
-    average_price FLOAT,
-    PRIMARY KEY(email_user, action_name),
-    FOREIGN KEY (email_user) REFERENCES users(email),
-    FOREIGN KEY (action_name) REFERENCES action(name)
+    average_price DECIMAL(10,2),
+    FOREIGN KEY (id_user) REFERENCES users(id),
+    FOREIGN KEY (id_action) REFERENCES action(id)
 );
 
 CREATE TABLE action_history (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     action_name VARCHAR(255),
     date DATE,
-    price_on_this_date FLOAT,
-    PRIMARY KEY(action_name, date),
-    FOREIGN KEY (action_name) REFERENCES action(name)
+    price_at_this_date FLOAT,
+    FOREIGN KEY (action_name) REFERENCES action(id)
 );
 
 CREATE TABLE news (
-    title VARCHAR(255) PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255),
     text TEXT,
-    impacted_action_name VARCHAR(255),
-    action_variation FLOAT,
-    FOREIGN KEY (impacted_action_name) REFERENCES action(name)
+    impacted_action_id VARCHAR(255),
+    FOREIGN KEY (impacted_action_id) REFERENCES action(id)
 );
 
 CREATE TABLE follows (
-    follower VARCHAR(255),
-    followed VARCHAR(255),
-    PRIMARY KEY(follower, followed),
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_follower VARCHAR(255),
+    id_followed VARCHAR(255),
     FOREIGN KEY (follower) REFERENCES users(email),
     FOREIGN KEY (followed) REFERENCES users(email)
 );
