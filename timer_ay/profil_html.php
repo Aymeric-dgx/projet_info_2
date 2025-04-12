8dd55586ad37c08841db40f7dcc00af36f4155a2
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2025-04-12 15:23:50 +0200

    Update profil_html.php

--- timer_ay/profil_html.php
+++ timer_ay/profil_html.php
@@ -5,14 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script>
-        if ('serviceWorker' in navigator) {
+        if('serviceWorker' in navigator) {
             navigator.serviceWorker.register('service_worker.js')
-                .then(() => console.log("Service worker enregistré !"))
-                .catch(e => console.error("Erreur service worker :", e));
-}
+        }
     </script>
 </head>
 <body>
-    <a href="2eme_page.html">Deuxieme page</a>
+
 </body>
 </html>
