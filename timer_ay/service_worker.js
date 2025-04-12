ce3fee2aeb290bbd90b4ba060818294713bfbb10
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2025-04-12 10:53:41 +0200

    Create service_worker.js

---
+++ timer_ay/service_worker.js
@@ -0,0 +1,19 @@
+let timer;
+
+const updatePeriod = 2 * 60 * 1000; // 2 minutes
+
+
+async function update_loop() {
+  fetch("update.php");
+}
+  
+  
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+  setInterval(update_loop, 5000); // Démarre la boucle au moment de l’activation
+});
