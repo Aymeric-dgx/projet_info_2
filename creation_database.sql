19bf0ce8a8fe40aba56f958f51892e3a002837f3
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2025-04-29 17:34:26 +0200

    Update creation_database.sql

--- creation_database.sql
+++ creation_database.sql
@@ -17,8 +17,8 @@ CREATE TABLE action (
     nom VARCHAR(255) NOT NULL,
     symbole VARCHAR(255) NOT NULL UNIQUE,
     description TEXT NOT NULL,
-    actual_price DECIMAL(10,2) NOT NULL,
-    original_price DECIMAL(10,2) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,    /* Prix actuel */
+    original_price DECIMAL(10,2) NOT NULL,    /* Prix d'origine, à "t=0" */
     id_dividende INT,
     FOREIGN KEY (id_dividende) REFERENCES dividende(id)
  );
