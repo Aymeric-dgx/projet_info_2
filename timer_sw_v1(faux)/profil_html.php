5fd960baea8014a374412af485af26395c14989f
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2025-04-30 10:13:18 +0200

    Add files via upload

---
+++ timer_sw_v1(faux)/profil_html.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script>
+        if('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('service_worker.js')
+        }
+    </script>
+</head>
+<body>
+
+</body>
+</html>
