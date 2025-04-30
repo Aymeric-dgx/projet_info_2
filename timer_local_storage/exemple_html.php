c2fa5dedbd1bc775d95269d64e8099a36ce133ae
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2025-04-30 10:31:09 +0200

    Update and rename search_html.php to exemple_html.php

---
+++ timer_local_storage/exemple_html.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="launch_update.js"></script> <!--- Ligne à rajouter afin de lier le fichier js à la page, à faire pour chacune d'entre elles --->
+</head>
+<body>
+    
+</body>
+</html>
